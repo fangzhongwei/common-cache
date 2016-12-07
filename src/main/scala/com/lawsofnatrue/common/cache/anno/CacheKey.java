0fb3ca242ea0b9894d766d9f3cbd6464e554995e
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-07 12:05:26 +0800

    framework

--- src/main/scala/com/lawsofnatrue/common/cache/anno/CacheKey.java
+++ src/main/scala/com/lawsofnatrue/common/cache/anno/CacheKey.java
@@ -1,7 +1,14 @@
 package com.lawsofnatrue.common.cache.anno;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by fangzhongwei on 2016/12/7.
  */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CacheKey {
 }
--- src/main/scala/com/lawsofnatrue/common/cache/anno/ServiceCache.java
+++ src/main/scala/com/lawsofnatrue/common/cache/anno/ServiceCache.java
@@ -2,9 +2,16 @@ package com.lawsofnatrue.common.cache.anno;
 
 import com.lawsofnatrue.common.cache.enumeration.CacheMethod;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by fangzhongwei on 2016/12/7.
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ServiceCache {
     CacheMethod method();
     String keyDir() default "";
